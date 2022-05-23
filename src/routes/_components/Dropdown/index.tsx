ee066856454010788d545a02fa4d534272f5c7ab
jihun1233 <leejihun1233@gmail.com>
2022-05-24 04:56:11 +0900

    Update: border, marker 조절기능 추가
    - props로 커스텀하여 border, marker 선택
    
    - unbordered를 true로 하면 테두리 없이 사용할 수 있도록 함(버튼만 적용, list부분은 테두리 있음)
    - isMarked를 true로 하면 button과 li에 텍스트 옆에 마커가 렌더링
    - markColors를 넘겨줘야 색이 지정됨 형식은 {아이템이름 : 색상}

--- src/routes/_components/Dropdown/index.tsx
+++ src/routes/_components/Dropdown/index.tsx
@@ -1,17 +1,34 @@
 import { MouseEvent, useEffect, useRef, useState } from 'react'
-import { DropdownMarker } from 'assets/svgs'
+import { DropdownIcon } from 'assets/svgs'
 import { cx } from 'styles'
 import { useClickAway } from 'react-use'
 import useToggle from 'hooks/useToggle'
 import styles from './dropdown.module.scss'
 
+/** TODO:
+ *   border 추가
+ *  marker 색 추가
+ */
+interface IMarkColors {
+  [item: string]: string
+}
 interface Props {
   items: string[]
   onItemChange: (itemValue: string) => void
   size?: 'normal' | 'big'
+  unbordered?: boolean
+  isMarked?: boolean
+  markColors?: IMarkColors
 }
 
-const Dropdown = ({ items, onItemChange, size = 'normal' }: Props) => {
+const Dropdown = ({
+  items,
+  onItemChange,
+  size = 'normal',
+  unbordered = false,
+  isMarked = false,
+  markColors = {},
+}: Props) => {
   const [selectedItem, setSelectedItem] = useState(items[0])
   const [isSelected, setIsSelected] = useState(false)
   const [showList, toggleShowList, , closeList] = useToggle()
@@ -37,15 +54,22 @@ const Dropdown = ({ items, onItemChange, size = 'normal' }: Props) => {
 
   return (
     <div className={cx(styles.container, { [styles.big]: size === 'big' })} ref={clickAwayRef}>
-      <button type='button' onClick={toggleShowList}>
+      <button type='button' onClick={toggleShowList} className={cx({ [styles.unbordered]: unbordered })}>
+        {isMarked && (
+          <div className={cx(styles.marker)} style={{ backgroundColor: markColors[selectedItem] ?? 'transparent' }} />
+        )}
         <p>{selectedItem}</p>
-        <div className={styles.dropdownMarkerBox}>
-          <DropdownMarker />
+        <div className={styles.dropdownIconBox}>
+          <DropdownIcon />
         </div>
       </button>
+
       <ul className={cx({ [styles.show]: showList })}>
         {items.map((item) => (
           <li key={`list-item-${item}`} role='row' onClick={handleItemClick} data-value={item}>
+            {isMarked && (
+              <div className={styles.marker} style={{ backgroundColor: markColors[item] ?? 'transparent' }} />
+            )}
             <p>{item}</p>
           </li>
         ))}
--- src/routes/_components/index.tsx
+++ src/routes/_components/index.tsx
@@ -1,6 +1,15 @@
 import Dropdown from './Dropdown'
 
 const DROPDOWN_ITEMS = ['매드업', 'ROAS', '광고비', '노출 수', '클릭 수', '전환 수', '매출']
+const MARK_COLORS = {
+  매드업: '#111111',
+  ROAS: '#4FADF7',
+  광고비: '#525252',
+  '노출 수': '#a5b5cc',
+  '클릭 수': '#85DA47',
+  '전환 수': '#deff84',
+  매출: '#ff70c8',
+}
 
 const Components = () => {
   const handleItemChange = (item: string) => {
@@ -8,8 +17,14 @@ const Components = () => {
   }
   return (
     <div>
-      <Dropdown size='normal' items={DROPDOWN_ITEMS} onItemChange={handleItemChange} />
+      big
       <Dropdown size='big' items={DROPDOWN_ITEMS} onItemChange={handleItemChange} />
+      normal
+      <Dropdown size='normal' items={DROPDOWN_ITEMS} onItemChange={handleItemChange} />
+      unbordered
+      <Dropdown size='normal' items={DROPDOWN_ITEMS} onItemChange={handleItemChange} unbordered />
+      marked
+      <Dropdown size='big' items={DROPDOWN_ITEMS} onItemChange={handleItemChange} isMarked markColors={MARK_COLORS} />
     </div>
   )
 }
