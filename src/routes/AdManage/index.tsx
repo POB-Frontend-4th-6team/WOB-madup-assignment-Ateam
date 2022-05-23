c5195f272bd94ffdfe57f5b9c017285e58812e73
Chiho <minepanorama@naver.com>
2022-05-23 22:45:19 +0900

    Add: New button on ad-management page

--- src/routes/AdManage/index.tsx
+++ src/routes/AdManage/index.tsx
@@ -5,6 +5,9 @@ import Card from './Card/index'
 const AdManage = () => {
   return (
     <div className={styles.container}>
+      <button type='button' className={styles.makeAdButton}>
+        광고 만들기
+      </button>
       <Card />
     </div>
   )
