c70226df65d2b06d2dc598eb2228f0efa3cce4a1
rkddbs1031 <rkddbs1031@conbridge.co.kr>
2022-05-23 16:56:39 +0900

    ADD: Sidebar 적용 및 scss 수정

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,16 +3,18 @@ import { Routes, Route } from 'react-router-dom'
 import styles from './routes.module.scss'
 import Dashboard from './Dashboard'
 import AdManage from './AdManage'
+import Sidebar from './_components/Sidebar'
 
 const App = () => {
   return (
-    <div className={styles.app}>
+    <main className={styles.app}>
+      <Sidebar />
       <Routes>
         <Route path='/' element={<Dashboard />} />
         <Route path='adManage' element={<AdManage />} />
         <Route path='*' element={<div>404</div>} />
       </Routes>
-    </div>
+    </main>
   )
 }
 
