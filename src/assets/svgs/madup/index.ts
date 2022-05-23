adba74422055e2d77ce8944d13138ac13e852d51
rkddbs1031 <rkddbs1031@conbridge.co.kr>
2022-05-23 16:54:55 +0900

    ADD: Sidebar svg 추가

---
+++ src/assets/svgs/madup/index.ts
@@ -0,0 +1,4 @@
+export { ReactComponent as Logo } from './logo.svg'
+export { ReactComponent as DashBoard } from './dashboard.svg'
+export { ReactComponent as AdManage } from './adManage.svg'
+export { ReactComponent as GuideIcon } from './guide.svg'
