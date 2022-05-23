bd987707241014ede48be9bbac30b62d688b6f4e
rkddbs1031 <rkddbs1031@gamil.com>
2022-05-23 22:32:43 +0900

    ADD: Header 컴포넌트 & scss 추가

--- src/routes/_components/Header/index.tsx
+++ src/routes/_components/Header/index.tsx
@@ -1,9 +1,22 @@
-import { Alarm, Profile, Setting } from 'assets/svgs/madup'
+import { useAppSelector, useAppDispatch } from 'hooks'
+
+import { Alarm, Hamburger, Profile, Setting } from 'assets/svgs/madup'
+import { getSidebarDrawer, setSidebar } from 'states/sidebar'
 import styles from './header.module.scss'
 
 const Header = () => {
+  const sidebarDrawer = useAppSelector(getSidebarDrawer)
+  const dispatch = useAppDispatch()
+
+  const handleSidebarShow = () => {
+    dispatch(setSidebar(!sidebarDrawer))
+  }
+
   return (
     <header>
+      <button type='button' onClick={handleSidebarShow} className={styles.hamburger}>
+        <Hamburger />
+      </button>
       <div className={styles.container}>
         <ul>
           <li>
