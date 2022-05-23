0cb11e59c404876b8f1bf006bc84780ea2f2cdb5
rkddbs1031 <rkddbs1031@gamin.com>
2022-05-23 17:40:23 +0900

    ADD: Header 컴포넌트 적용

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,11 +3,13 @@ import { Routes, Route } from 'react-router-dom'
 import styles from './routes.module.scss'
 import Dashboard from './Dashboard'
 import AdManage from './AdManage'
+import Header from './_components/Header'
 import Sidebar from './_components/Sidebar'
 
 const App = () => {
   return (
     <main className={styles.app}>
+      <Header />
       <Sidebar />
       <Routes>
         <Route path='/' element={<Dashboard />} />
