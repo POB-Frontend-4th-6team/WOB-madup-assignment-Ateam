cd63da88cc4424ac86e75f81df9442a60bee33c3
rkddbs1031 <rkddbs1031@gamil.com>
2022-05-23 22:32:33 +0900

    ADD: 사이드바 redux 추가

---
+++ src/states/sidebar.ts
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import type { RootState } from '.'
+
+export interface ISidebarState {
+  sidebarShow: boolean
+}
+
+const INITAL_STATE: ISidebarState = {
+  sidebarShow: false,
+}
+
+export const sidebarDrawerSlice = createSlice({
+  name: 'dieaseSearch',
+  initialState: INITAL_STATE,
+  reducers: {
+    setSidebar: (state: ISidebarState, action: PayloadAction<boolean>) => {
+      state.sidebarShow = action.payload
+    },
+  },
+})
+export const { setSidebar } = sidebarDrawerSlice.actions
+
+export const getSidebarDrawer = (state: RootState): boolean => state.sidebar.sidebarShow
+
+export default sidebarDrawerSlice.reducer
