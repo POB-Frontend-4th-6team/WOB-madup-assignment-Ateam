523ddb37a395235f1cf5518c1995100d602ac6dd
rkddbs1031 <rkddbs1031@gamin.com>
2022-05-23 17:39:14 +0900

    ADD: Header 컴포넌트 및 scss 추가

--- src/routes/_components/Header/index.tsx
+++ src/routes/_components/Header/index.tsx
@@ -1,7 +1,25 @@
+import { Alarm, Profile, Setting } from 'assets/svgs/madup'
 import styles from './header.module.scss'
 
 const Header = () => {
-  return <div className={styles.container}>Header</div>
+  return (
+    <header>
+      <div className={styles.container}>
+        <ul>
+          <li>
+            <Alarm />
+          </li>
+          <li>
+            <Setting />
+          </li>
+          <li>
+            <Profile />
+            <span>원티드님</span>
+          </li>
+        </ul>
+      </div>
+    </header>
+  )
 }
 
 export default Header
