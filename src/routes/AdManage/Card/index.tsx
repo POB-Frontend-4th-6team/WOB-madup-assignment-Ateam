8d1a385fe5a7fa4f8bf94073b91ed6699143a1f3
Chiho <minepanorama@naver.com>
2022-05-25 00:00:53 +0900

    ADD: JSON 데이터 연산을 위한 커스텀 함수 추가

--- src/routes/AdManage/Card/index.tsx
+++ src/routes/AdManage/Card/index.tsx
@@ -1,30 +1,61 @@
 import styles from './card.module.scss'
+import { useAppSelector } from 'hooks'
 
 import { advertisementCardType } from 'types/ad.d'
+import { getStatusOption } from 'states/ad'
+import {
+  fetchAllAd,
+  fetchOnlyActiveAd,
+  fetchOnlyEndedAd,
+  englishToKorean,
+  startDate,
+  dayBudget,
+  totalSales,
+} from 'services/ad'
 
-const index = (props: advertisementCardType) => {
+const Card = () => {
+  const statusOption = useAppSelector(getStatusOption)
+
+  let fetchedAdData: advertisementCardType[]
+
+  if (statusOption === 'All') fetchedAdData = fetchAllAd()
+  if (statusOption === 'Active') fetchedAdData = fetchOnlyActiveAd()
+  if (statusOption === 'Ended') fetchedAdData = fetchOnlyEndedAd()
+
+  const ValidCardList: any = () => {
+    const validCard = fetchedAdData.map((ad) => {
+      return (
+        <section className={styles.cardContainer} key={`advertisement_${ad.title}`}>
+          <h1 className={styles.cardHeader}>{ad.title}</h1>
+          <dl>
+            <dt>상태</dt>
+            <dd>{englishToKorean(ad.status)}</dd>
+            <dt>광고 생성일</dt>
+            <dd>{startDate(ad.startDate)}</dd>
+            <dt>일 희망 예산</dt>
+            <dd>{dayBudget(ad.budget)}만원</dd>
+            <dt>광고 수익률</dt>
+            <dd>{ad.report.roas}%</dd>
+            <dt>매출</dt>
+            <dd>{totalSales(ad.report.roas, ad.report.cost)}</dd>
+            <dt>광고 비용</dt>
+            <dd>{ad.report.cost}</dd>
+          </dl>
+          <button type='button' className={styles.editButton}>
+            수정하기
+          </button>
+        </section>
+      )
+    })
+
+    return validCard
+  }
+
   return (
-    <section className={styles.cardContainer}>
-      <h1 className={styles.cardHeader}>웹광고_20210601123030</h1>
-      <dl>
-        <dt>상태</dt>
-        <dd>진행중</dd>
-        <dt>광고 생성일</dt>
-        <dd>2021-06-15</dd>
-        <dt>일 희망 예산</dt>
-        <dd>40만원</dd>
-        <dt>광고 수익률</dt>
-        <dd>694%</dd>
-        <dt>매출</dt>
-        <dd>26,071만원</dd>
-        <dt>광고 비용</dt>
-        <dd>3,759만원</dd>
-      </dl>
-      <button type='button' className={styles.editButton}>
-        수정하기
-      </button>
-    </section>
+    <div>
+      <ValidCardList />
+    </div>
   )
 }
 
-export default index
+export default Card
