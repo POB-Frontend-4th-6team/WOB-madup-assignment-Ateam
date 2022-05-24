2f1b7dbc303f4bf513a8753476d2c96ec4f7a1ef
이지훈 <leejihun1233@gmail.com>
2022-05-24 19:27:19 +0900

    Feature/shared components (#3)
    * Add: Dropdown 컴포넌트 작성
    
    - select 태그의 option을 담는 컨테이너 부분의 스타일을 수정하지 못해 details,summary와 ul,li를 사용
    
    - route의 기본 라우팅에 Components로 이동하도록 하고 Components로 공용 component들을 볼 수 있도록 함
    - dropdown marker svg추가, 텍스트 overflow시 svg크기가 비정상적으로 작아지는 현상이 있어 div태그로 감싸 크기 고정
    - props로 size를 받아 선택할 수 있도록 className으로 구분
    
    * Update: Dropdown props 및 onItemChange  추가
    
    - props로 size, items, onItemChange를 받아오도록 설정
    
    - onItemChange로 아이템을 클릭시 해당 아이템의 값을 사용해 콜백을 실행하고 내부적으로 selected값이 변경되어 출력되도록
      ->isSelected를 사용해 최초렌더링시엔 콜백 실행안되도록 함
    
    * Add: useToggle hook 추가, 태그 변경
    
    - toggle 사용하는 부분을 useToggle 훅으로 분리
    
    - item을 클릭할 때, 외부 클릭할 때 list 토글 동작을 추가하면서 기존의 details, summary의 기본적인 open, close 동작이 의미없어지게 되어 button과 div로 수정
    
    * Update: 태그 변경, 변수 추가
    
    - details, summary -> div, button으로 변경한 것 css 수정
    
    - Dropdown 관련 colors, levels 변수 추가
    
    * Update: Components 라우트 수정, routes/index 확장자 수정
    
    - default가 아닌 /components로 라우팅하도록 수정
    
    - routes/index의 확장자를 jsx -> tsx로 수정
    
    * Update: border, marker 조절기능 추가
    
    - props로 커스텀하여 border, marker 선택
    
    - unbordered를 true로 하면 테두리 없이 사용할 수 있도록 함(버튼만 적용, list부분은 테두리 있음)
    - isMarked를 true로 하면 button과 li에 텍스트 옆에 마커가 렌더링
    - markColors를 넘겨줘야 색이 지정됨 형식은 {아이템이름 : 색상}
    
    * fix: isMarked를 markColors를 통해 알아내도록 수정
    
    - isMarked와 markColors가 중복되는 기능이 있어 isMarked를 Props에서 삭제
    
    - markColors의 key가 하나이상 있으면 isMarked를 true로 인식하도록 로컬변수로 만듦
    
    * ✨Add: ContentsContainer추가
    
    - 컨텐츠를 감싸주는 흰 배경
    
    - 🐛 위쪽 margin이 적용되지않고 헤더밑에 딱붙어있는데 헤더가 fix고 아래 app이 margin-top으로 밀려나있어서 그거랑 겹치면서 안되는것같다
    
    * Update: colors 변수로 설정, app 상단 margin->padding 수정

---
+++ src/routes/_components/index.tsx
@@ -0,0 +1,35 @@
+import ContentsContainer from './ContentsContainer'
+import Dropdown from './Dropdown'
+
+const DROPDOWN_ITEMS = ['매드업', 'ROAS', '광고비', '노출 수', '클릭 수', '전환 수', '매출']
+const MARK_COLORS = {
+  매드업: '#111111',
+  ROAS: '#4FADF7',
+  광고비: '#525252',
+  '노출 수': '#a5b5cc',
+  '클릭 수': '#85DA47',
+  '전환 수': '#deff84',
+  매출: '#ff70c8',
+}
+
+const Components = () => {
+  const handleItemChange = (item: string) => {
+    console.log(item)
+  }
+  return (
+    <div>
+      <ContentsContainer>
+        big
+        <Dropdown size='big' items={DROPDOWN_ITEMS} onItemChange={handleItemChange} />
+        normal
+        <Dropdown size='normal' items={DROPDOWN_ITEMS} onItemChange={handleItemChange} />
+        unbordered
+        <Dropdown size='normal' items={DROPDOWN_ITEMS} onItemChange={handleItemChange} unbordered />
+        marked
+        <Dropdown size='big' items={DROPDOWN_ITEMS} onItemChange={handleItemChange} markColors={MARK_COLORS} />
+      </ContentsContainer>
+    </div>
+  )
+}
+
+export default Components
