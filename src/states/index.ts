cb7e4cf33456fa6e0eced76ea6fd094603bd8599
Chiho <minepanorama@naver.com>
2022-05-24 23:59:47 +0900

    ADD: 광고 현황 Redux 구현

--- src/states/index.ts
+++ src/states/index.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import sidebar from './sidebar'
+import ad from './ad'
 
 export const store = configureStore({
   reducer: {
+    ad,
     sidebar,
   },
   devTools: process.env.NODE_ENV !== 'production',
