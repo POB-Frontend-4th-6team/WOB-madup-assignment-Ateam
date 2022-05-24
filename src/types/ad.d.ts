3da41f63c72c58166e79daa21efc6fff12ac556e
Chiho <minepanorama@naver.com>
2022-05-25 00:01:45 +0900

    FIX: 광고 카드 타입 옵셔널 체이닝 해제

--- src/types/ad.d.ts
+++ src/types/ad.d.ts
@@ -1,15 +1,15 @@
 export interface advertisementCardType {
-  id?: number
-  adType?: string
-  title?: string
-  budget?: number
-  status?: string
-  startDate?: string
-  endDate?: null | string
-  report?: {
-    cost?: number
-    convValue?: number
-    roas?: number
+  id: number
+  adType: string
+  title: string
+  budget: number
+  status: string
+  startDate: string
+  endDate: null | string
+  report: {
+    cost: number
+    convValue: number
+    roas: number
   }
 }
 
