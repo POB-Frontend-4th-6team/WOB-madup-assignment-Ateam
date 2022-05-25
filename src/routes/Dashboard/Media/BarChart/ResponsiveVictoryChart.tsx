3fcbbf046110e38070f823c9e48adfe28f9148c4
김학률 <52916848+markyul@users.noreply.github.com>
2022-05-26 07:09:40 +0900

    Feature: media 기능 추가 (#15)
    - datePicker에 따라 그래프 수정
    - 디자인 수정

--- src/routes/Dashboard/Media/BarChart/ResponsiveVictoryChart.tsx
+++ src/routes/Dashboard/Media/BarChart/ResponsiveVictoryChart.tsx
@@ -1,8 +1,8 @@
 import { ReactNode, useRef } from 'react'
+import { round } from 'lodash'
+import { VictoryChart, VictoryTooltip, VictoryVoronoiContainer } from 'victory'
 
 import { useEffect, useState } from 'hooks'
-import { VictoryChart, VictoryTheme, VictoryTooltip, VictoryVoronoiContainer } from 'victory'
-import { round } from 'lodash'
 
 const getSize = () => {
   let width
@@ -38,7 +38,22 @@ const ResponsiveVictoryChart = ({ children }: IVictoryChart) => {
   const ref = useRef<HTMLDivElement>(null)
   const { width } = useSize()
 
-  const props = {
+  const containerComponent = (
+    <VictoryVoronoiContainer
+      labels={({ datum }) => `${round(datum.value, 2)}`}
+      responsive={false}
+      labelComponent={
+        <VictoryTooltip
+          style={{ fill: 'white', fontSize: 14 }}
+          flyoutStyle={{ fill: '#3a474e' }}
+          flyoutHeight={40}
+          flyoutPadding={20}
+        />
+      }
+    />
+  )
+
+  const chartProps = {
     children,
     width,
   }
@@ -46,25 +61,10 @@ const ResponsiveVictoryChart = ({ children }: IVictoryChart) => {
   return (
     <div ref={ref}>
       <VictoryChart
-        theme={VictoryTheme.material}
+        height={350}
         domainPadding={{ x: 100, y: 0 }}
-        {...props}
-        containerComponent={
-          <VictoryVoronoiContainer
-            labels={({ datum }) => {
-              return `${round(datum.value, 2)}`
-            }}
-            responsive={false}
-            labelComponent={
-              <VictoryTooltip
-                style={{ fill: 'white', fontSize: 14 }}
-                flyoutStyle={{ fill: '#3a474e' }}
-                flyoutHeight={40}
-                flyoutPadding={20}
-              />
-            }
-          />
-        }
+        containerComponent={containerComponent}
+        {...chartProps}
       />
     </div>
   )
