a7b833fd854ec5cc690bdcfa1d7acdb9013172c9
jihun1233 <leejihun1233@gmail.com>
2022-05-26 06:59:16 +0900

    Delete: 불필요한 파일 제거

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom'
 import styles from './routes.module.scss'
 import Dashboard from './Dashboard'
 import AdManage from './AdManage'
-import Components from './_components'
 import Header from './_components/Header'
 import Sidebar from './_components/Sidebar'
 
@@ -15,7 +14,6 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Dashboard />} />
         <Route path='adManage' element={<AdManage />} />
-        <Route path='components' element={<Components />} />
         <Route path='*' element={<div>404</div>} />
       </Routes>
     </main>
