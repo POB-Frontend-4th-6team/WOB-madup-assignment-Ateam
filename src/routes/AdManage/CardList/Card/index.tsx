7d43f140fcb5fc2be364a34e22cd7106a1551063
Chiho <minepanorama@naver.com>
2022-05-25 16:41:26 +0900

    FIX: 광고 생성일에 종료일 추가, 웹, 앱 광고 분리

--- src/routes/AdManage/CardList/Card/index.tsx
+++ src/routes/AdManage/CardList/Card/index.tsx
@@ -1,7 +1,7 @@
 import styles from './card.module.scss'
 
 import { advertisementCardType } from 'types/ad.d'
-import { englishToKorean, startDate, dayBudget, totalSales } from 'services/ad'
+import { sortWebAndAppAd, englishToKorean, startDate, dayBudget, totalSales } from 'services/ad'
 
 interface Props {
   info: advertisementCardType
@@ -10,12 +10,12 @@ interface Props {
 const Card = ({ info }: Props) => {
   return (
     <section className={styles.cardContainer}>
-      <h1 className={styles.cardHeader}>{info.title}</h1>
+      <h1 className={styles.cardHeader}>{sortWebAndAppAd(info.adType, info.title)}</h1>
       <dl>
         <dt>상태</dt>
         <dd>{englishToKorean(info.status)}</dd>
         <dt>광고 생성일</dt>
-        <dd>{startDate(info.startDate)}</dd>
+        <dd>{startDate(info.startDate, info.endDate)}</dd>
         <dt>일 희망 예산</dt>
         <dd>{dayBudget(info.budget)}만원</dd>
         <dt>광고 수익률</dt>
--- src/services/ad.ts
+++ src/services/ad.ts
@@ -14,16 +14,37 @@ export const fetchOnlyEndedAd = () => {
   return ALL_DATA.filter((ad) => ad.status === 'ended')
 }
 
+export const sortWebAndAppAd = (type: string, title: string) => {
+  if (type === 'web') return `Web_${title}`
+
+  return `App_${title}`
+}
+
 export const englishToKorean = (props: string) => {
   if (props === 'active') return '진행중'
 
   return '종료'
 }
 
-export const startDate = (props: string) => {
-  const dayAndTime = props.split('T')
+export const startDate = (start: string, end: string | null) => {
+  const cleanYearMaker = (str: string) => {
+    return str.substr(2)
+  }
 
-  return dayAndTime[0]
+  const startDayAndTime = start.split('T')
+
+  if (end === null) {
+    return cleanYearMaker(startDayAndTime[0])
+  }
+
+  if (end !== null) {
+    const EndDayAndTime = end.split('T')
+    const cleanStartYear = cleanYearMaker(startDayAndTime[0])
+    const cleanEndYear = cleanYearMaker(EndDayAndTime[0])
+    return `${cleanStartYear} (${cleanEndYear})`
+  }
+
+  return startDayAndTime[0]
 }
 
 export const dayBudget = (props: number) => {
