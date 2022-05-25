314983c962771005744d085a06f8f91ff50c2d60
Chiko <87627359+usernamechiho@users.noreply.github.com>
2022-05-25 13:35:25 +0900

    Feature: Ad-Management UI ( no responsive UI yet ) (#6)
    * Delete: i18n.babel file
    
    * Add: add JSON data locally
    
    * Design: Made card for ad-management
    
    * Add: New button on ad-management page
    
    * ADD: 광고 카드 타입 추가
    
    * MOD: 타입 export 파일 수정
    
    * ADD: JSON파일 타입 추가
    
    * ADD: 광고 현황 Redux 구현
    
    * ADD: JSON 데이터 연산을 위한 커스텀 함수 추가
    
    * FIX: 광고 카드 타입 옵셔널 체이닝 해제
    
    * FEATURE: Ad-Management UI (반응형 X) 완성
    
    * FIX: 사이드바 svg width, height 24px로 고정
    
    * FIX: Card 컴포넌트 분리
    
    * ADD: 페이지 헤더 추가, 페이지 헤더 색상 constants/_colors.scss에 추가
    
    * FIX: 카드리스트, 카드 컴포넌트 분리

--- src/states/ad.ts
+++ src/states/ad.ts
@@ -1,1 +1,26 @@
-export {}
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import type { RootState } from '.'
+
+export interface StatusOptionState {
+  optionState: string
+}
+
+const INITIAL_STATE: StatusOptionState = {
+  optionState: 'All',
+}
+
+export const statusOptionSlice = createSlice({
+  name: 'statusOption',
+  initialState: INITIAL_STATE,
+  reducers: {
+    setStatusOption: (state: StatusOptionState, action: PayloadAction<string>) => {
+      state.optionState = action.payload
+    },
+  },
+})
+
+export const { setStatusOption } = statusOptionSlice.actions
+
+export const getStatusOption = (state: RootState): string => state.ad.optionState
+
+export default statusOptionSlice.reducer
--- src/types/ad.d.ts
+++ src/types/ad.d.ts
@@ -1,1 +1,19 @@
-// 광고 타입
+export interface advertisementCardType {
+  id: number
+  adType: string
+  title: string
+  budget: number
+  status: string
+  startDate: string
+  endDate: null | string
+  report: {
+    cost: number
+    convValue: number
+    roas: number
+  }
+}
+
+interface responseDataType {
+  count: number
+  ads: adCardInfo[]
+}
