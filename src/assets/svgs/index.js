f9c0fc2643e5c1a503fddb3b6ee166681524cb6d
jihun1233 <leejihun1233@gmail.com>
2022-05-25 20:30:21 +0900

    Add: svg 추가 및 madup폴더로 이동

--- src/assets/svgs/index.js
+++ src/assets/svgs/index.js
@@ -1,3 +1,2 @@
 export { ReactComponent as LogoImage } from './logo.svg'
 export { ReactComponent as CheckIcon } from './check.svg'
-export { ReactComponent as DropdownIcon } from './dropdownIcon.svg'
