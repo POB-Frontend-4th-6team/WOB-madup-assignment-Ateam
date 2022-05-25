af4fcf46ff9ecb985d71503a6122bb91b307f154
Chiho <minepanorama@naver.com>
2022-05-26 02:49:06 +0900

    ADD: 컴포넌트 로딩 추가

--- src/routes/AdManage/index.tsx
+++ src/routes/AdManage/index.tsx
@@ -1,15 +1,24 @@
 import styles from './adManage.module.scss'
 
-import { useAppDispatch } from 'hooks'
+import { useAppDispatch, useState, useMount } from 'hooks'
 import { setStatusOption } from 'states/ad'
 
 import ContentsContainer from 'routes/_components/ContentsContainer'
 import Dropdown from 'routes/_components/Dropdown'
 import CardList from './CardList'
+import Loading from 'routes/_components/Loading'
 
 const DROPDOWN_ITEMS = ['전체보기', '진행중', '종료']
 
 const AdManage = () => {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useMount(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 2000)
+  })
+
   const dispatch = useAppDispatch()
 
   const dispatchCurrentAdState = (item: string) => {
@@ -22,16 +31,19 @@ const AdManage = () => {
   return (
     <>
       <h1 className={styles.pageHeader}>광고관리</h1>
-      <ContentsContainer>
-        <div className={styles.dropdownAndButtonContainer}>
-          <Dropdown items={DROPDOWN_ITEMS} onItemChange={dispatchCurrentAdState} />
-          <button type='button' className={styles.makeAdButton}>
-            광고 만들기
-          </button>
-        </div>
+      {isLoading && <Loading size={150} color='#3a474e' style={{ margin: '50px 0 0 50px' }} />}
+      {!isLoading && (
+        <ContentsContainer>
+          <div className={styles.dropdownAndButtonContainer}>
+            <Dropdown items={DROPDOWN_ITEMS} onItemChange={dispatchCurrentAdState} />
+            <button type='button' className={styles.makeAdButton}>
+              광고 만들기
+            </button>
+          </div>
 
-        <CardList />
-      </ContentsContainer>
+          <CardList />
+        </ContentsContainer>
+      )}
     </>
   )
 }
